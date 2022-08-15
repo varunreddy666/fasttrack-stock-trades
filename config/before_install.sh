f9ebbb87ec4fe273457754d7e368adc291b307f9
varunreddy666 <48866348+varunreddy666@users.noreply.github.com>
2022-08-15 15:27:19 +0530

    fixed the script

--- config/before_install.sh
+++ config/before_install.sh
@@ -4,6 +4,4 @@ cd /home/ec2-user/application/server/
 now="$(date '+%Y%m%d%H%M%S')"
 <<<<<<< HEAD
 sudo mv stocktrades.jar /home/ec2-user/application/archive/stocktrades_$now.jar
-=======
-sudo mv stocktrades_$now.jar /home/ec2-user/application/archive
->>>>>>> 50761584f5ba6beaa4d8b7492f52b1e466522857
+
