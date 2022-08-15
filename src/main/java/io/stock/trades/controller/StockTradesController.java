3d3244438923863fcd89ece0f6186af8f269a9ba
varunreddy666 <48866348+varunreddy666@users.noreply.github.com>
2022-08-15 21:49:44 +0530

    log4j configuration

--- src/main/java/io/stock/trades/controller/StockTradesController.java
+++ src/main/java/io/stock/trades/controller/StockTradesController.java
@@ -1,13 +1,17 @@
 package io.stock.trades.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockTradesController {
 
+	Logger logger = LogManager.getLogger(StockTradesController.class);
 	@GetMapping("/stocks")
 	public String getMessage() {
+		logger.info("stocks api is getting called..(getMessage)");
 		return "Welcome to stocktrades.io ";
 	}
 	
